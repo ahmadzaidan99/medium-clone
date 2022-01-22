212b5ec852a754e2ad215b033cdb3291962ed82c
ahmadzaidan99 <zaidan.ahmad@hotmail.com>
2022-01-23 01:20:35 +0300

    Finished Build

---
+++ pages/index.tsx
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import Posts from "../components/Posts";
+import {Post} from "../typings";
+import {sanityClient} from "../sanity";
+
+interface Props {
+  posts: Post[];
+}
+
+export default function Home({posts}: Props) {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Head>
+        <title>Medium Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Banner />
+      <Posts posts={posts} />
+    </div>
+  );
+}
+export async function getServerSideProps() {
+  const query = `*[_type=="post"]{
+        _id,
+      title,
+        slug,
+      author ->{
+        name,
+        image
+      },
+      description,
+      mainImage
+      }`;
+  console.log("post");
+  const posts = await sanityClient.fetch(query);
+  return {
+    props: {
+      posts,
+    },
+  };
+}
